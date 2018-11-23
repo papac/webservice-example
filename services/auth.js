9f9a2970730501dab8036942da263067bede0f85
Franck DAKIA <dakiafranck@gmail.com>
2018-11-23 10:21:36 +0000

    change app structure

--- services/auth.js
+++ services/auth.js
@@ -1,12 +1,131 @@
 const express = require('express');
 const logger = require('morgan');
-const authRouter = require('../routes/auth');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const mongoose = require('mongoose');
+const expiresIn = 86400;
+const bcrypt = require('bcryptjs');
+const User = require('../model/user');
+
 const app = express();
+
+// Connect mongo db
+mongoose.connect(config.mongodb, {
+  useNewUrlParser: true, 
+  useCreateIndex: true
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', authRouter);
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (typeof email === 'undefined') {
+    return res.send({
+      message: 'email or password is not valide !',
+      error: true
+    });
+  }
+
+  if (typeof password === 'undefined') {
+    return res.send({
+      message: 'email or password is not valide !',
+      error: true
+    });
+  }
+  
+  User.findOne({ email }, (err, user) => {
+    if (err || !bcrypt.compareSync(password, user.password)) {
+      return res.status(500).send({
+        message: 'email or password is not valide !',
+        error: true
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        name: user.name,
+        email, 
+        exp: expiresIn
+      }, 
+      config.secret
+    );
+
+    res.send({
+      message: 'authenticated',
+      error: false, 
+      token,
+      expiresIn
+    });
+  });
+});
+
+app.post('/register', (req, res) => {
+  const { email, password, name } = req.body;
+
+  const data = {
+    name: name,
+    email: email, 
+    password: bcrypt.hashSync(password)
+  };
+
+  User.create(data, (err, user) => {
+    if (err) {
+      return res.status(500).send({
+        message: 'email or password is invalide !',
+        error: true
+      });
+    }
+
+    const token = jwt.sign(      {
+      id: user._id,
+      name: user.name,
+      email: user.email, 
+      exp: expiresIn
+    }, config.secret);
+
+    res.send({
+      message: 'authenticated',
+      error: false, 
+      token,
+      expiresIn
+    });
+  })
+});
+
+app.post('/verify', (req, res) => {
+  const token = req.headers['x-access-token'];
+
+  if (typeof token === 'undefined') {
+    return res.send({message: 'Token is undefined', error: false});
+  }
+
+  jwt.verify(token.trim(), config.secret, (err, decode) => {
+    if (err) {
+      switch (err.name) {
+        case 'TokenExpiredError': {
+          return res.status(500).send({message: 'Token is expirated', error: true})
+        }
+        case 'JsonWebTokenError': {
+          return res.status(500).send({message: 'An error in your Token ', error: true})
+        }
+        default: {
+          return res.status(500).send({message: 'Token is invalide', error: true});
+        }
+      }
+    }
+
+    User.findById(decode._id, err => {
+      if (err) {
+        return res.status(500).send({message: 'Token is invalide', error: true});
+      }
+
+      return res.send({message: "Ok", error: false, token, decode});
+    });
+  });
+});
 
 module.exports = app;
--- services/mailer.js
+++ services/mailer.js
@@ -1,12 +1,100 @@
 const express = require('express');
 const logger = require('morgan');
-const mailerRouter = require('../routes/mailer');
+const nodemailer = require('nodemailer');
+const config = require('../config');
+const mongoose = require('mongoose');
+const reEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const verifyToken = require('../middleware/verify-token');
+
 const app = express();
+
+// Connect mongo db
+mongoose.connect(config.mongodb, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+// Load verifiy token middleware
+app.use(verifyToken(
+  require('../model/user')
+));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', mailerRouter);
+const checkEmail = (email) => reEmail.test(email);
+
+app.post('/verify', (req, res) => {
+  const { email } = req.body;
+
+  if (typeof email === 'undefined') {
+    res.send({
+      message: 'Please add email for verification', 
+      error: true
+    });
+  }
+
+  if (checkEmail(email)) {
+    return res.send({
+      message: "Email is valide", 
+      error: false
+    });
+  }
+
+  return res.send({
+    message: 'Email is not valide', 
+    error: true
+  });
+});
+
+app.post('/process', (req, res) => {
+  
+  const transporter = nodemailer.createTransport({
+    host: 'localhost',
+    port: 1025,
+    ignoreTLS: true,
+    secure: false
+  });
+
+  let validation = [];
+
+  // Make validation
+  for (field of ['subject', 'to', 'text']) {
+    if (typeof req.body[field] === 'undefined') {
+      validation.push(field);
+    }
+  }
+
+  // Check validation
+  if (validation.length > 0) {
+    return res.send({
+      message: 'You have omit ' + validation.join(', '), 
+      error: true
+    });
+  }
+  
+  // Destructurate body 
+  const { subject, to, text } = req.body;
+  const form = req.body.form || process.env.EMAIL_FROM || 'mailer@test.dev';
+  
+  // Formatage message
+  const message = {
+    from: form,
+    to: to,
+    subject: subject,
+    html: text
+  };
+
+  // Send email
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.error(err);
+      return res.send({message: "An error occured", error: true});
+    }
+
+    res.send({message: 'Message sent', error: false});
+  });
+});
 
 module.exports = app;
